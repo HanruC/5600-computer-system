d0786734ba76115c9e2f14e24d27a648d24fb24e
HanruC <chen.hanru@northeastern.edu>
2024-03-26 12:34:20 -0400

    all good

--- LearnC/mpg2km.h
+++
@@ -1,8 +0,0 @@
-#ifndef MPG2KM_H
-#define MPG2KM_H
-
-double mpg2kml(double mpg);
-double mpg2lphm(double mpg);
-double lph2mpg(double lph);
-
-#endif 
--- README.md
+++
@@ -1,3 +0,0 @@
-Homework for CS5600 - Computer System and Opertaing System 
-Using C 
-Linux or MacOS Terminal 
